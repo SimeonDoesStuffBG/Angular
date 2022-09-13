4464c4f9115a55f27e6f05aa8d11585422fdfafb
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-09-13 23:56:06 +0300

    Made sign in form

--- character_creator/src/app/app.module.ts
+++ character_creator/src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { MyInputComponent } from './components/my-input/my-input.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyInputComponent,
+    SignInComponent,
+    
   ],
   imports: [
     BrowserModule,
